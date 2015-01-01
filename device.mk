61d5ffb67dee7ac7d62806512b990b4c26957bb0
root <justinjams@gmail.com>
2015-01-01 00:50:52 -0500

    move stuff from vendor/cm to toro device folder

--- device.mk
+++ device.mk
@@ -25,6 +25,10 @@ PRODUCT_COPY_FILES += \
 	device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml \
 	frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
 
+
+PRODUCT_COPY_FILES += \
+        device/samsung/toro/prebuilt/etc/init.d/99airplanemodelistener:system/etc/init.d/99airplanemodelistener
+
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.vc_call_vol_steps=7
 
