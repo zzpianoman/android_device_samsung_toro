bd1c6d33980b122ff2a94e96e93d5c71f67dccf4
Justin M <justinjams@gmail.com>
2015-07-30 04:01:33 -0400

    Misc Fixes

--- cm.mk
+++ cm.mk
@@ -5,18 +5,13 @@ PRODUCT_RELEASE_NAME := GN-CDMA
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/toro/aosp_toro.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := toro
-PRODUCT_NAME := cm_toro
+PRODUCT_NAME := aosp_toro
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := Samsung
